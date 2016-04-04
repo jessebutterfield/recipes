49fb1cdec96fd66b337487322430181afa7229b2
Jesse Butterfield <jesse.butterfield@gmail.com>
2016-04-04 15:20:01 -0700

    Added setup files and initial models

---
+++ mealplanner/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=200)
+    instruction = models.TextField()
+    servings = models.IntegerField()
+    
+    def __str__(self):
+        return str(self.name)
+    
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+    recipe = models.ForeignKey(Recipe)
+    quantity = models.FloatField()    
+    units = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return str(self.name)
+    
+class Meal(models.Model):
+    user = models.ForeignKey(User)
+    date = models.DateField()
+    servings = models.IntegerField()
+    recipe = models.ForeignKey(Recipe)
+
