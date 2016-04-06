1811ab7bf58f64b0d0cb5a6797c37875ef0a67d7
eguegan <eguegan@quantcast.com>
2016-04-06 13:24:28 -0700

    added author on recipe model.

--- mealplanner/models.py
+++ mealplanner/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Recipe(models.Model):
     name = models.CharField(max_length=200)
+    author = models.ForeignKey(User)
     instructions = models.TextField()
     servings = models.IntegerField()
     
