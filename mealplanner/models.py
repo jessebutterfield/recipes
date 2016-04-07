c1b9f7e6dffb1b8dfed3597120a210fed20a8e0e
Jesse Butterfield <jesse.butterfield@gmail.com>
2016-04-06 20:28:46 -0700

    Add to string method for meals and authors

--- mealplanner/models.py
+++ mealplanner/models.py
@@ -48,11 +48,17 @@ class Meal(models.Model):
     date = models.DateField()
     servings = models.IntegerField()
     recipe = models.ForeignKey(Recipe)
+    
+    def __str__(self):
+        return str(self.recipe.name)
 
 day_dictionary = list(zip(range(0,7),calendar.day_name))
 class Author(models.Model):
     user = models.OneToOneField(User)
     defaultServings = models.IntegerField(null=True, default = 4)
     firstDayOfWeek = models.IntegerField(choices = day_dictionary, default = 6)
+    
+    def __str__(self):
+        return str(self.user.username)
     
     
